6df05ed4491866db5575601ea6b336d32404e897
Ken Williams <kwilliams@cpan.org>
2000-02-11 04:48:15 +0000

    Now uses the logout() method.

--- t/eg/logout.pl
+++ t/eg/logout.pl
@@ -2,16 +2,12 @@ use strict;
 
 my $r = Apache->request;
 my $auth_type = $r->auth_type;
-my $auth_name = $r->auth_name;
-my $path = $r->dir_config($auth_name . "Path");
-$r->no_cache(1);
-$r->status(200);
-$r->header_out("Pragma", "no-cache");
-$r->header_out("Set-Cookie",$auth_type . "_" . $auth_name . "=; path=" .
-    $path . "; expires=Mon, 21-May-1971 00:00:00 GMT");
-$r->content_type("text/html");
-$r->send_http_header;
 
+# Delete the cookie, etc.
+$auth_type->logout($r);
+$r->content_type("text/html");
+$r->status(200);
+$r->send_http_header;
 
 print <<EOF;
 <HTML>
