ad71ff1bd892a26e576c36dc52fcb016b8a68f29
Michael Schout <mschout@cpan.org>
2002-06-21 04:13:19 +0000

    set VERSION to 3.02

--- AuthCookie.pm
+++ AuthCookie.pm
@@ -9,8 +9,8 @@ use Apache::AuthCookie::Util;
 use Apache::Util qw(escape_uri);
 use vars qw($VERSION);
 
-# $Id: AuthCookie.pm,v 2.31 2002-06-21 04:11:34 mschout Exp $
-$VERSION = '3.01';
+# $Id: AuthCookie.pm,v 2.32 2002-06-21 04:13:19 mschout Exp $
+$VERSION = '3.02';
 
 sub recognize_user ($$) {
   my ($self, $r) = @_;
@@ -883,7 +883,7 @@ implement anything, though.
 
 =head1 CVS REVISION
 
-$Id: AuthCookie.pm,v 2.31 2002-06-21 04:11:34 mschout Exp $
+$Id: AuthCookie.pm,v 2.32 2002-06-21 04:13:19 mschout Exp $
 
 =head1 AUTHOR
 
