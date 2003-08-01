cd09975d142867d50e1cb0d3396e382a99f134d8
Michael Schout <mschout@cpan.org>
2003-08-01 16:50:23 +0000

    update to run under mod_perl v2

--- t/htdocs/docs/echo_cookie.pl
+++ t/htdocs/docs/echo_cookie.pl
@@ -1,11 +1,18 @@
 use strict;
 
+use mod_perl;
+use constant MODPERL2 => ($mod_perl::VERSION >= 1.99);
+
+if (MODPERL2) {
+    require Apache::Access;
+}
+
 my $r = Apache->request;
 my $auth_type = $r->auth_type;
 
 # Delete the cookie, etc.
 $r->content_type("text/html");
 $r->status(200);
-$r->send_http_header;
+$r->send_http_header unless MODPERL2;
 
-print $auth_type->key;
+print $auth_type->key($r);
