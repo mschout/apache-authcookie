0d0ded674554b35746395fd8d6494fb285b60ad8
Michael Schout <mschout@cpan.org>
2006-06-20 15:43:00 +0000

    fix tiny pod documentation error

--- lib/Apache/AuthCookie.pm
+++ lib/Apache/AuthCookie.pm
@@ -9,7 +9,7 @@ use Apache::AuthCookie::Util;
 use Apache::Util qw(escape_uri);
 use vars qw($VERSION);
 
-# $Id: AuthCookie.pm,v 1.12 2006-06-05 01:12:51 mschout Exp $
+# $Id: AuthCookie.pm,v 1.13 2006-06-20 15:43:00 mschout Exp $
 $VERSION = '3.10';
 
 sub recognize_user ($$) {
@@ -982,7 +982,7 @@ method can set arbitrary entries in C<$r-E<gt>subprocess_env>.
 
 If you want to let users log themselves out (something that can't be
 done using Basic Auth), you need to create a logout script.  For an
-example, see t/eg/logout.pl.  Logout scripts may want to take
+example, see t/htdocs/docs/logout.pl.  Logout scripts may want to take
 advantage of AuthCookie's C<logout()> method, which will set the
 proper cookie headers in order to clear the user's cookie.  This
 usually looks like C<$r-E<gt>auth_type-E<gt>logout($r);>.
@@ -1032,7 +1032,7 @@ implement anything, though.
 
 =head1 CVS REVISION
 
-$Id: AuthCookie.pm,v 1.12 2006-06-05 01:12:51 mschout Exp $
+$Id: AuthCookie.pm,v 1.13 2006-06-20 15:43:00 mschout Exp $
 
 =head1 AUTHOR
 
